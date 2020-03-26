6e4a48ac13147831d60422e0e7227fe62f54f61b
Daniel Leal <37158127+daleal@users.noreply.github.com>
2020-03-26 15:44:39 -0300

    Minor documentation fixes (#79)
    * Now arrows between code don't move
    
    * Add smooth scrolling
    
    * Change hover color and fix js bug

---
+++ docs/assets/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $('#scrolly-button').on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        // window.location.hash = hash;
+      });
+    } // End if
+  });
+});
